f2d9ac812b6a24b360574d17cbf547d13725a93a
Ryan0980 <tusrau@gmail.com>
2022-10-24 02:20:29 -0400

    Add files via upload
    re newed

--- 20220511_hifini_lanzou.py
+++ 20220511_hifini_lanzou.py
@@ -1,30 +1,41 @@
 import time
 from selenium import webdriver
 
-#想法是输入hifini/lanzou的通用格式，下载到特定文件夹
+#想法是输入hifini的通用格式，下载到特定文件夹
 #比如：链接: https://www.lanzoui.com/i9zgwhe 提取码: i492
-#下载到download
+#下载到 E:\Music_Temp
 
 print("Started")
 print("please check if ChromeDriver version is correct")
 
-driver = webdriver.Chrome('xxxxxxxxxxxxxxx') #ChromeDriver location
-driver.get("https://www.lanzoui.com") 
+driver = webdriver.Edge('D:/apps/Music/Script/edgedriver_win64/msedgedriver.exe') #Driver location 浏览器下载位置
+#driver.get("https://www.lanzoui.com") 
 #check
+
+file1 = open('links.txt', 'rb')#links放在文本中，例子格式：
+#链接: https://hifini.lanzoui.com/iIVmKk4m54f 提取码: cbbk
+Lines = file1.readlines()
+count=0
 while 1:
-    val = input("Enter link: ")
-    print(val)
-    link=val.split()
-    print(link[1])
-    print(link[3])
-    
-    driver.get(link[1])
-    
-    driver.find_element_by_xpath('//*[@id="pwd"]').send_keys(link[3])
-    
+    for line in Lines:
+        count += 1
+        print("Line{}: {}".format(count, line.strip()))
 
-    driver.find_element_by_xpath('//*[@id="passwddiv"]/div/div[3]/div').click()
-    time.sleep(2)
+        #val = input("Enter link: ")
+        #print(val)
+        link=line.split()
+        print(link[1].decode())
+        print(link[3].decode())
+        
+        driver.get(link[1].decode())
+        
+        driver.find_element_by_xpath('//*[@id="pwd"]').send_keys(link[3].decode())
+        
 
-    driver.find_element_by_xpath('//*[@id="downajax"]/a').click()
+        driver.find_element_by_xpath('//*[@id="passwddiv"]/div/div[3]/div').click()
+        time.sleep(2)
+
+        driver.find_element_by_xpath('//*[@id="downajax"]/a').click()
+
+    time.sleep(20000000)
 
